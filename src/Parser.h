e47b4909a2a5397d362e376d2f276eadc62e16a4
Hilary Johnson <john1364@algonquinlive.com>
2023-04-13 10:28:32 -0400

    Added Parser files, and changed from Julius names to Phonon, including logo.

---
+++ src/Parser.h
@@ -0,0 +1,115 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2023
+* Author: Hilary Johnson, Isaac Ribeiro
+* Professors: Paulo Sousa
+************************************************************
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@                                                               @@@@
+@@          /        ==============================        /         @@
+@         ////     @@@@@ @@    @@@@@ @@@@@ @@@@@ @@@@@    ////        @
+@         ////*    @@ @@ @@    @@ @@ @@ @@ @@ @@ @@ @@   /////        @
+@     ((( ////)))) @@@@@ @@@@@ @@ @@ @@ @@ @@ @@ @@ @@///////( ///    @
+@     ((((, / )))))@@    @@ @@ @@@@@ @@ @@ @@@@@ @@ @@///// ,/////    @
+@     (((((/)))((    ------------------------------    ((((./////*    @
+@    //*./ /  .///   ---  PROGRAMMING LANGUAGE  ---  ////   / ( (//   @
+@    ///// / /////   ==============================  * ////* / ////   @
+@     ///// ///// ((                               (( ///// /////     @
+@    ((((   / , (((((                             (((((  //   /(((    @
+@    (((((((/ . (((((                          (((((* / (((((((       @
+@      (((((( //((((/((((                    *((((/((((/( (((((/      @
+@       .//,   * /   (((((                   (((((  ///    .//.       @
+@     ,////////./(  (((((* ////         (///(((((( ,/(,////////       @
+@         //////// ,// ((( /////,     ////// ((( //  /////// *        @
+@            (((((((((,// * /////     /////   (//(((((((((            @
+@            ((((((((((((/////         //.///  (((((((((((.           @
+@                   (///////// //(   (//.//////////                   @
+@                  (/////////             //////////                  @
+@                                                                     @
+@@          A L G O N Q U I N   C O L L E G E  -  2 0 2 3 W          @@
+@@@@                                                               @@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+*/
+
+/*
+************************************************************
+* File name: parser.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 � Compilers, Lab Section: [011, 012, 013]
+* Assignment: A32.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file is the main header for Parser (.h)
+*************************************************************/
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+/* Inclusion section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+/* Global vars */
+static Token lookahead;
+extern BufferReader* stringLiteralTable;
+extern phonon_intg line;
+extern Token tokenizer();
+extern phonon_char* keywordTable[];
+phonon_intg syntaxErrorNumber = 0;
+
+#define STR_LANGNAME	"phonon"
+#define LANG_WRTE		"print&"
+#define LANG_READ		"input&"
+#define LANG_MAIN		"main&"
+
+/* TO_DO: Create ALL constants for keywords (sequence given in table.h) */
+
+/* Constants */
+enum KEYWORDS {
+	NO_ATTR = -1,
+	KW_data,
+	KW_code,
+	KW_int,
+	KW_real,
+	KW_string,
+	KW_if,
+	KW_then,
+	KW_else,
+	KW_while,
+	KW_do
+};
+
+/* Function definitions */
+phonon_void startParser();
+phonon_void matchToken(phonon_intg, phonon_intg);
+phonon_void syncErrorHandler(phonon_intg);
+phonon_void printError();
+
+/* TO_DO: Place ALL non-terminal function declarations */
+phonon_void codeSession();
+phonon_void dataSession();
+phonon_void optVarListDeclarations();
+phonon_void optionalStatements();
+phonon_void outputStatement();
+phonon_void outputVariableList();
+phonon_void program();
+phonon_void statement();
+phonon_void statements();
+phonon_void statementsPrime();
+
+#endif
--- test-reader.bat
+++ test-reader.bat
@@ -1,41 +1,77 @@
-echo off
+:: COMPILERS COURSE - SCRIPT ---------------------------------------------
+:: SCRIPT A22 - CST8152 - Winter 2023
+
+
 cls
+SET COMPILER=PhononCompiler.exe
 
-echo: & echo: & echo: & echo:
-echo ********** Running test: Empty file ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_empty.txt
-echo: & pause
 
-echo: & echo: & echo: & echo:
-echo ********** Running test: Hello World ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_hello-world.txt
-echo: & pause
+SET FILE1=test_empty
+SET FILE2=test_err
+SET FILE3=test_general
+SET FILE4=test_large
+SET FILE5=main.txt
+SET FILE6=Test.txt
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: One character ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_one.txt
-echo: & pause
+SET ASSIGNMENT=A22
+SET EXTENSION=ph
+SET OUTPUT=out
+SET ERROR=err
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: Six characters ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_six.txt
-echo: & pause
+SET PARAM=S
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: Large file (FIXED MODE) ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_large.txt
-echo: & pause
+:: ---------------------------------------------------------------------
+:: Begin of Tests (A22 - W22) ------------------------------------------
+:: ---------------------------------------------------------------------
+@echo off
+ECHO "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ECHO "@@@@                                                               @@@@"
+ECHO "@@          /        ==============================        /         @@"
+ECHO "@         ////     @@@@@ @@    @@@@@ @@@@@ @@@@@ @@@@@    ////        @"
+ECHO "@         ////*    @@ @@ @@    @@ @@ @@ @@ @@ @@ @@ @@   /////        @"
+ECHO "@     ((( ////)))) @@@@@ @@@@@ @@ @@ @@ @@ @@ @@ @@ @@///////( ///    @"
+ECHO "@     ((((, / )))))@@    @@ @@ @@@@@ @@ @@ @@@@@ @@ @@///// ,/////    @"
+ECHO "@     (((((/)))((    ------------------------------    ((((./////*    @"
+ECHO "@    //*./ /  .///   ---  PROGRAMMING LANGUAGE  ---  ////   / ( (//   @"
+ECHO "@    ///// / /////   ==============================  * ////* / ////   @"
+ECHO "@     ///// ///// ((                               (( ///// /////     @"
+ECHO "@    ((((   / , (((((                             (((((  //   /(((    @"
+ECHO "@    (((((((/ . (((((                          (((((* / (((((((       @"
+ECHO "@      (((((( //((((/((((                    *((((/((((/( (((((/      @"
+ECHO "@       .//,   * /   (((((                   (((((  ///    .//.       @"
+ECHO "@     ,////////./(  (((((* ////         (///(((((( ,/(,////////       @"
+ECHO "@         //////// ,// ((( /////,     ////// ((( //  /////// *        @"
+ECHO "@            (((((((((,// * /////     /////   (//(((((((((            @"
+ECHO "@            ((((((((((((/////         //.///  (((((((((((.           @"
+ECHO "@                   (///////// //(   (//.//////////                   @"
+ECHO "@                  (/////////             //////////                  @"
+ECHO "@                                                                     @"
+ECHO "@@          A L G O N Q U I N   C O L L E G E  -  2 0 2 3 W          @@"
+ECHO "@@@@                                                               @@@@"
+ECHO "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ECHO "                                                                       "
+ECHO "[SCANNER SCRIPT ......................................................]"
+ECHO "                                                                       "
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: Large file (ADDITIVE MODE) ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_large.txt A 1 10
-echo: & pause
+ren *.exe %COMPILER%
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: Large file (MULTIPLY MODE) ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_large.txt M 1 2
-echo: & pause
+::
+:: BASIC TESTS  ----------------------------------------------------------
+::
+:: Basic Tests (A22 - W22) - - - - - - - - - - - - - - - - - - - - - -
 
-echo: & echo: & echo: & echo:
-echo ********** Runnning test: Invalid characters ********** & echo:
-PhononCompiler.exe R test-inputs/reader/test_invalid.txt
+%COMPILER% %PARAM% %FILE1%.%EXTENSION%	> %FILE1%-%ASSIGNMENT%.%OUTPUT%	2> %FILE1%-%ASSIGNMENT%.%ERROR%
+%COMPILER% %PARAM% %FILE2%.%EXTENSION%	> %FILE2%-%ASSIGNMENT%.%OUTPUT%	2> %FILE2%-%ASSIGNMENT%.%ERROR%
+%COMPILER% %PARAM% %FILE3%.%EXTENSION%	> %FILE3%-%ASSIGNMENT%.%OUTPUT%	2> %FILE3%-%ASSIGNMENT%.%ERROR%
+%COMPILER% %PARAM% %FILE4%.%EXTENSION%	> %FILE4%-%ASSIGNMENT%.%OUTPUT%	2> %FILE4%-%ASSIGNMENT%.%ERROR%
+%COMPILER% %PARAM% %FILE5%.%EXTENSION%	> %FILE5%-%ASSIGNMENT%.%OUTPUT%	2> %FILE5%-%ASSIGNMENT%.%ERROR%
+%COMPILER% %PARAM% %FILE6%.%EXTENSION%	> %FILE6%-%ASSIGNMENT%.%OUTPUT%	2> %FILE6%-%ASSIGNMENT%.%ERROR%
+
+:: SHOW OUTPUTS - - - - - - - - - - - - - - - - - - - - - - - - - - -
+DIR *.OUT
+DIR *.ERR
+
+
+:: ---------------------------------------------------------------------
+:: End of Tests (A22 - W23) --------------------------------------------
+:: ---------------------------------------------------------------------
